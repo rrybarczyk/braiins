14ec5081fe25e2543c03fab6db99278a5c669057
Jan Čapek <jan@braiins.com>
2020-05-12 10:56:52 +0200

    bos: toolbox: Don't change git config on windows when handling symlinks
    - remove git-win-symlinks.sh as modifying users' global git-config
      with new aliases is not desirable
    
    - inline simplified rm/checkout functions into the build script

--- braiins-os/bos-toolbox/build-bos-toolbox.sh
+++ braiins-os/bos-toolbox/build-bos-toolbox.sh
@@ -9,6 +9,48 @@ set -e
     exit 1
 }
 
+__win_git_rm_symlinks() {
+    git ls-files -s . | grep -E '^120000' | cut -f2 |
+	while IFS= read -r symlink; do
+	    case "$symlink" in
+		(*/*) symdir=${symlink%/*} ;;
+		(*) symdir=. ;;
+	    esac
+
+	    git checkout -- "$symlink"
+	    src="${symdir}/$(cat "$symlink")"
+	    
+	    posix_to_dos_sed='s_^/\([A-Za-z]\)_\1:_;s_/_\\\\_g'
+	    doslnk=$(printf '%s\n' "$symlink" | sed "$posix_to_dos_sed")
+	    dossrc=$(printf '%s\n' "$src" | sed "$posix_to_dos_sed")
+
+	    if [ -f "$src" ]; then
+		rm -f "$symlink"
+		cmd //C mklink //H "$doslnk" "$dossrc"
+	    elif [ -d "$src" ]; then
+		rm -f "$symlink"
+		cmd //C mklink //J "$doslnk" "$dossrc"
+	    else
+		printf 'error: git-rm-symlink: Not a valid source\n' >&2
+		printf '%s =/=> %s  (%s =/=> %s)...\n' \
+		       "$symlink" "$src" "$doslnk" "$dossrc" >&2
+		false
+	    fi || printf 'ESC[%d]: %d\n' "$ppid" "$?"
+
+	    git update-index --assume-unchanged "$symlink"
+	done
+}
+
+__win_git_checkout_symlinks() {
+    git ls-files -s | grep -E '^120000' | cut -f2 | while IFS= read -r symlink; do
+	git update-index --no-assume-unchanged "$symlink"
+	rmdir "$symlink" >/dev/null 2>&1
+	git checkout -- "$symlink"
+	printf 'Restored git symlink: %s -> %s\n' "$symlink" "$(cat "$symlink")"
+    done
+}
+
+
 EMBEDDED_BOS_RELEASE=$(basename ${1%.tar.gz})
 echo $EMBEDDED_BOS_RELEASE > bos-version.txt
 tar xvzf $1
@@ -26,7 +68,7 @@ if [ x$MSYSTEM = xMINGW64 ]; then
     VIRTUAL_ENV_ARGS=
     VIRTUAL_ENV_BIN=Scripts
     ICON_ARG="--icon bos${BOS_VARIANT}.ico"
-    git rm-symlinks
+    __win_git_rm_symlinks
 else
     PYINST_PATH_SEP=':'
     VIRTUAL_ENV_ARGS=--python=/usr/bin/python3
@@ -60,5 +102,5 @@ pyinstaller $ICON_ARG -F $DATA_ARGS bos-toolbox.py --name bos${BOS_VARIANT}-tool
 
 # Cleanup the converted symlinks on Windows
 if [ x$MSYSTEM = xMINGW64 ]; then
-    git checkout-symlinks
+    __win_git_checkout_symlinks
 fi
