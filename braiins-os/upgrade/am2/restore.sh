d1b7367bc26cf613455755ebc336e89a95ec707d
Libor Vašíček <libor.vasicek@braiins.com>
2020-05-22 20:35:25 +0200

    bos: Create restore script for new Antminer firmwares (S15, S17, ...)
    - the script requires correctly set MTD partitions and firmware files
      which are available on Bitmain's website:
      https://service.bitmain.com/support/download

--- braiins-os/upgrade/am2/restore.sh
+++ braiins-os/upgrade/am2/restore.sh
@@ -22,26 +22,43 @@
 # of such proprietary license or if you have any other questions, please
 # contact us at opensource@braiins.com.
 
+# The script expects following setting of MTD partitions:
+# recovery_mtdparts=mtdparts=pl35x-nand:40m(BOOT.bin-env-dts-kernel),32m(ramfs),8m(configs),16m(reserve),32m(ramfs-bak),128m(reserve1)
+
 set -e
 
 # write all images to NAND
 mtd -e BOOT.bin-env-dts-kernel write ./BOOT.bin BOOT.bin-env-dts-kernel
-mtd -np 0x1020000 write ./devicetree.dtb BOOT.bin-env-dts-kernel
-mtd -np 0x1100000 write ./uImage BOOT.bin-env-dts-kernel
-mtd -np 0x1040000 write ./upgrade-marker.bin BOOT.bin-env-dts-kernel
+mtd -np 0x1A00000 write ./devicetree.dtb BOOT.bin-env-dts-kernel
+mtd -np 0x2000000 write ./uImage BOOT.bin-env-dts-kernel
 
-mtd -e upgrade-rootfs write ./rootfs.jffs2 upgrade-rootfs
+mtd -e ramfs-bak write ./uramdisk.image.gz ramfs
+mtd -e ramfs-bak write ./uramdisk.image.gz ramfs-bak
 
-mtd erase angstram-rootfs
+mtd erase configs
+mtd erase reserve
+mtd erase reserve1
 sync
 
+ubiformat /dev/mtd2
 ubiattach -p /dev/mtd2
-mount -t ubifs ubi0:rootfs /mnt
+ubimkvol /dev/ubi0 -N configs -m
+ubiupdatevol -t /dev/ubi0_0
+
+mount -t ubifs ubi0:configs /mnt
 
 # restore configuration
 tar xvzf ./config.tar.gz
-mv ./config /mnt/home/usr_config
+mv ./config/* /mnt/
 
 umount /mnt
 ubidetach -p /dev/mtd2
+
+# format partition with nvdata
+ubiformat /dev/mtd5
+ubiattach -p /dev/mtd5
+ubimkvol /dev/ubi0 -N reserve1 -m
+ubiupdatevol -t /dev/ubi0_0
+ubidetach -p /dev/mtd5
+
 sync
