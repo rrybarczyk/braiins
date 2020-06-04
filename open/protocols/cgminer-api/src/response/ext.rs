0dc8da5b49bcef1deb5eb78c53232bee8c8b1488
Jakub Horak <jakub.horak@braiins.com>
2020-06-04 14:15:56 +0200

    cgminer-api: Rename `LoadedProfileFrom` -> `LoadedProfileCreatedOn`

--- open/protocols/cgminer-api/src/response/ext.rs
+++ open/protocols/cgminer-api/src/response/ext.rs
@@ -145,8 +145,8 @@ pub struct TunerChainStatus {
     pub approx_consumption: u32,
     #[serde(rename = "TunerRunning")]
     pub tuner_running: bool,
-    #[serde(rename = "LoadedProfileFrom")]
-    pub loaded_profile_from: Option<Time>,
+    #[serde(rename = "LoadedProfileCreatedOn")]
+    pub loaded_profile_created_on: Option<Time>,
 }
 
 /// Basic temperature control settings
